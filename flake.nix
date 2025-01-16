711aa81ca7e37d5edf2469cedd994a0474aa9066
rbbhn <matthew.mccaman@gmail.com>
2025-01-16 15:15:29 -0600

    changed filestructure

--- flake.nix
+++ flake.nix
@@ -4,18 +4,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"; # NixOS official package source, using the nixos-24.11 branch here;
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    catppuccin.url = "github:catppuccin/nix";
     ghostty.url = "github:ghostty-org/ghostty";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
+  # Laptop -----------------------------------------------------
     nixosConfigurations.wing = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
-        ./configuration.nix
-        inputs.nixos-hardware.nixosModules.framework-13-7040-amd
-        inputs.catppuccin.nixosModules.catppuccin
+        ./hosts/wing/configuration.nix
+      ];
+    };
+  # Desktop ----------------------------------------------------
+    nixosConfigurations.nest = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/nest/configuration.nix
       ];
     };
   };

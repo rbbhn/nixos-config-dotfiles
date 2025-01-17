2ee68292205486b0cb06bd489a258a6336940ccd
rbbhn <matthew.mccaman@gmail.com>
2025-01-17 12:38:08 -0600

    change around programs a bit

--- modules/nixOS/extrautils.nix
+++ modules/nixOS/extrautils.nix
@@ -9,19 +9,15 @@
     #Move to user zone
     git
     openjdk # java >:(
-    fzf # "fzf" - command line fuzzy finder. ess: interactive grep
+    fzf # "fzf" - command line fuzzy finder & search utility
     ripgrep # "rg" - faster, configurable grep
-    zoxide # "z" - faster, configurable cd w/ teleportation
+    zoxide # "z" and "zi" - faster, configurable cd w/ teleportation (interactive = "zi")
     bat # "bat" - faster, configurable cat
     eza # "eza" - faster, configurable ls
+    tldr # "tldr" - manpages, summarized
+    lazygit # "lg" - terminal UI for git
   ];
 
   #move to user zone
   programs.fish.interactiveShellInit = "zoxide init fish | source"; # enable zoxide in fish
-  programs.fish.shellAliases = {
-    ls = "eza -la --octal-permissions --git";
-    cat = "bat";
-    grep = "rg";
-    fzf = "fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'";
-  };
 }

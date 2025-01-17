99fce026128b2cc0d441ea0f74063f3f93d60879
rbbhn <matthew.mccaman@gmail.com>
2025-01-17 13:41:13 -0600

    swap to greetd

--- modules/nixOS/boot/displaymanager.nix
+++ modules/nixOS/boot/displaymanager.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   programs.regreet = {
     enable = true;
-    font.package = pkgs.(nerdfonts.override { fonts = [ "Terminus" ]; });
+    font.package = (pkgs.nerdfonts.override { fonts = [ "Terminus" ]; });
     font.name  = "Terminess Nerd Font Mono";
     font.size = 16;
     theme.package = pkgs.breeze-gtk;

146b831e79c9981693c6cf147c52828704b70387
rbbhn <matthew.mccaman@gmail.com>
2025-01-17 17:30:40 -0600

    introduced home manager

--- flake.nix
+++ flake.nix
@@ -3,15 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"; # NixOS official package source, using the nixos-24.11 branch here
-    #home-manager.url = "github:nix-community/home-manager";
-    #home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     ghostty.url = "github:ghostty-org/ghostty";
     stylix.url = "github:danth/stylix";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: let
-    stateVersion = "";
+    stateVersion = "24.11";
     system = "x86_64-linux";
     username = "rbbhn";
     laptop = "wing";
@@ -21,7 +21,7 @@
 
       # Laptop -----------------------------------------------------
       ${laptop} = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+        system = "${system}";
         specialArgs = let
           hostname = "${laptop}";
         in {
@@ -36,26 +36,26 @@
         };
         modules = [
           ./hosts/wing/configuration.nix
-          #home-manager.nixosModules.home-manager {
-          #  home-manager.useGlobalPkgs = true;
-          #  home-manager.useUserPackages = true;
-          #  home-manager.extraSpecialArgs = {
-          #    inherit inputs;
-          #    hostname = "wing";
-          #  };
-          #  home-manager.users.rbbhn = import ./modules/home/rbbhn;
-          #}
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = {
+              inherit inputs;
+              hostname = "${hostname}";
+            };
+            home-manager.users.${username} = import ./modules/home/${username}.nix;
+          }
         ];
       };
 
       # Desktop ----------------------------------------------------
-      ${desktop} = nixpkgs.lib.nixosSystem {
+      /*${desktop} = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/nest/configuration.nix
         ];
-      };
+      };*/
 
     };
   };
---
+++ modules/home/rbbhn.nix
@@ -0,0 +1,23 @@
+{ pkgs, inputs, username, ... }: {
+  #imports = [
+  #    inputs.nvf.homeManagerModules.default
+  #    inputs.niri.homeModules.niri
+  #
+  #    ./fastfetch
+  #    ./fuzzel
+  #    ./shell
+  #    ./nvf
+  #    ./spotify-tui
+  #  ]
+  #  ++ lib.optional toad ./wayland;
+
+  home.username = "${username}";
+  home.homeDirectory = "/home/${username}";
+
+  home.packages = with pkgs; [
+    pipes
+  ];
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}

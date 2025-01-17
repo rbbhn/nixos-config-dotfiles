0f56c1b1a5e416c0c4199e639d7a2d5a3bd412df
rbbhn <matthew.mccaman@gmail.com>
2025-01-17 16:29:13 -0600

    home manager fuckery

--- modules/users.nix
+++ modules/users.nix
@@ -1,11 +1,15 @@
-{  pkgs, ... }: {
+{  pkgs, inputs, ... }: {
   users.users.rbbhn = { # Define my user account
     isNormalUser = true;
     description = "Matthew M";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.fish;
   };
-  inputs.home-manager.nixosModules.home-manager { # Define my user home-manager config
-    home-manager.users.rbbhn = import ./home/rbhhn;
+  outputs = {pkgs, inputs.home-manager, ...}@inputs: {
+    inputs.home-manager.nixosModules.home-manager { # Define my user home-manager config
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users.rbbhn = import ./home/rbhhn;
+    };
   };
 }

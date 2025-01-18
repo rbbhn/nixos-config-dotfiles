e4e5e575e3f644b3396c212a73a8ab8f44f12829
rbbhn <matthew.mccaman@gmail.com>
2025-01-18 13:49:53 -0600

    autogenerate wallpaper (stylix)

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -1,7 +1,16 @@
-{ pkgs, stylix, ... }: {
-  stylix.enable = true;
-  stylix.autoEnable = false;
+{ pkgs, stylix, ... }: let
+  theme = "${pkgs.base16-schemes}/share/themes/mocha.yaml";
+  wallpaper = pkgs.runCommand "image.png" {} ''
+    COLOR=$(${pkgs.yq}/bin/yq -r .base00 ${theme})
+    COLOR="#"$COLOR
+    ${pkgs.imagemagick}/bin/magick -size 2256x1504 xc:$COLOR $out
+  '';
+in {
+  stylix = {
+    enable = true;
+    autoEnable = false;
 
-  stylix.image = ./wallpaper.png;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/mocha.yaml";
+    image = wallpaper;
+    base16Scheme = theme;
+  };
 }

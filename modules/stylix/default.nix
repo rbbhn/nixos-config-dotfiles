78e5a78e20ac136e806cecd277314d235b945461
rbbhn <matthew.mccaman@gmail.com>
2025-01-17 21:07:31 -0600

    Beginning stylix v5

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -1,15 +1,16 @@
 { pkgs, ... }: {
   stylix = {
-  enable = true;
-  image = ./wallpaper.png;
-  base16 = "${pkgs.base16-schemes}/share/themes/brushtrees-dark.yaml";
-  fonts = {
-    monospace = {
-      (pkgs.nerdfonts.override { fonts = [ "Terminus" ]; });
-      name = "Terminess Nerd Font Mono";
+    enable = true;
+    image = ./wallpaper.png;
+    base16 = "${pkgs.base16-schemes}/share/themes/brushtrees-dark.yaml";
+    fonts = {
+      monospace = {
+        package = (pkgs.nerdfonts.override { fonts = [ "Terminus" ]; });
+        name = "Terminess Nerd Font Mono";
+      };
+      serif = config.stylix.fonts.monospace;
+      sansSerif = config.stylix.fonts.monospace;
+      emoji = config.stylix.fonts.monospace;
     };
-    serif = config.stylix.fonts.monospace;
-    sansSerif = config.stylix.fonts.monospace;
-    emoji = config.stylix.fonts.monospace;
   };
 }

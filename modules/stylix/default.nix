cc41cf1bb0d055f150ea2dfdac80e37cbdb72bcf
rbbhn <matthew.mccaman@gmail.com>
2025-01-18 13:37:00 -0600

    updated stylix module

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -1,31 +1,7 @@
-{ stylix, ... }: {
+{ pkgs, stylix, ... }: {
   stylix.enable = true;
   stylix.autoEnable = false;
 
   stylix.image = ./wallpaper.png;
-  stylix.base24Scheme = {
-    system = "base24";
-    slug = "espresso";
-    name = "Espresso";
-    author = "FredHappyface (https://github.com/fredHappyface)";
-    variant = "dark";
-    palette = {
-      base00 = "#262626";
-      base01 = "#343434";
-      base02 = "#535353";
-      base03 = "#797979";
-      base04 = "#a0a09f";
-      base05 = "#c7c7c5";
-      base06 = "#eeeeec";
-      base07 = "#ffffff";
-      base08 = "#d25151";
-      base09 = "#ffc66d";
-      base0A = "#8ab7d9";
-      base0B = "#a5c261";
-      base0C = "#bed6ff";
-      base0D = "#6c99bb";
-      base0E = "#d197d9";
-      base0F = "#692828";
-    };
-  };
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/mocha.yaml";
 }

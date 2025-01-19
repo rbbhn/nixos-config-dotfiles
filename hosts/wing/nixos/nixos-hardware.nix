82784e1b490c3d4b94d7eb30ddc6c952873c2e8e
rbbhn <matthew.mccaman@gmail.com>
2025-01-19 14:52:12 -0600

    Restructuring

--- hosts/wing/nixos/nixos-hardware.nix
+++ hosts/wing/nixos/nixos-hardware.nix
@@ -1,6 +1,6 @@
-{ nixos-hardware, ... }: {
+{ inputs, ... }: {
   imports = [
-    nixos-hardware.nixosModules.framework-13-7040-amd
+    inputs.nixos-hardware.nixosModules.framework-13-7040-amd
   ];
 
   services.fwupd.enable = true; # Enable Framework BIOS updates

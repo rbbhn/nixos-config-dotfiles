dadb797f599ba54d3067ada93a5a60ea08033a40
rbbhn <matthew.mccaman@gmail.com>
2025-01-23 18:10:02 -0600

    (fixed?) fish auto-init starship

--- home-manager/starship.nix
+++ home-manager/starship.nix
@@ -3,7 +3,7 @@
     enable = true;
     enableFishIntegration = true;
   };
-  programs.fish.shellInit = "starship init fish | source";
+  programs.fish.interactiveShellInit = "starship init fish | source";
 
   imports = [
     ./${username}/starship-config.nix

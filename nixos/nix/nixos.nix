5ff3f5d0b603cba2ab9de8a32aa67e8a5288317c
rbbhn <matthew.mccaman@gmail.com>
2025-01-25 13:50:33 -0600

    Moved /nixos/nix to /nixos/common/nix

--- nixos/nix/nixos.nix
+++
@@ -1,4 +0,0 @@
-{ stateVersion, ... }: {
-  system.stateVersion = "${stateVersion}"; # Do NOT change (unless self-sabatoge will actually going to help somehow)
-  nix.settings.experimental-features = [ "nix-command" "flakes" ]; # Enable Flakes & accompanying new nix command-line tool
-}

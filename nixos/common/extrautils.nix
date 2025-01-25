43fb8d368d626ff1e57877d76a4142dc639d3e83
rbbhn <matthew.mccaman@gmail.com>
2025-01-25 14:47:59 -0600

    Added back default programs??

--- nixos/common/extrautils.nix
+++ nixos/common/extrautils.nix
@@ -3,5 +3,22 @@
     # These packages remain UNCONFIGURED - if needed, separate into modules and configure.
     git
     wget
+    # Pull progs. out and make individual modules when programs gain home-manager configs
+    # TUI
+    progress
+    lazygit
+    ranger
+    gh
+    glow
+
+    # GUI
+    kdePackages.kate
+    spotify
+    keepassxc
+    vlc
+    ghostty
   ];
+
+  # Install apps
+  programs.firefox.enable = true;
 }

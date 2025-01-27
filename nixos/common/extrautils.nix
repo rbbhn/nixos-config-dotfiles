c0c1c10aa27d22ddd0c26aab9a0fc3b478d32177
rbbhn <matthew.mccaman@gmail.com>
2025-01-27 14:16:07 -0600

    Add logseq electron EOL dependency overlay (fix)

--- nixos/common/extrautils.nix
+++ nixos/common/extrautils.nix
@@ -22,6 +22,23 @@
     logseq
   ];
 
+  #Logseq fix
+  nixpkgs.overlays = [
+  (
+    final: prev: {
+      logseq = prev.logseq.overrideAttrs (oldAttrs: {
+        postFixup = ''
+          makeWrapper ${prev.electron_20}/bin/electron $out/bin/${oldAttrs.pname} \
+            --set "LOCAL_GIT_DIRECTORY" ${prev.git} \
+            --add-flags $out/share/${oldAttrs.pname}/resources/app \
+            --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
+            --prefix LD_LIBRARY_PATH : "${prev.lib.makeLibraryPath [ prev.stdenv.cc.cc.lib ]}"
+        '';
+      });
+    }
+  )
+  ];
+
   # Install apps
   programs.firefox.enable = true;
 }

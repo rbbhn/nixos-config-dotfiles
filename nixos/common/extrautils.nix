a771e905e819da250ca264318fb8e12742644778
rbbhn <matthew.mccaman@gmail.com>
2025-01-27 14:20:58 -0600

    Add nixpkgs unstable source, add logseq from unstable repo

--- nixos/common/extrautils.nix
+++ nixos/common/extrautils.nix
@@ -19,24 +19,7 @@
     keepassxc
     vlc
     ghostty
-    logseq
-  ];
-
-  #Logseq fix
-  nixpkgs.overlays = [
-  (
-    final: prev: {
-      logseq = prev.logseq.overrideAttrs (oldAttrs: {
-        postFixup = ''
-          makeWrapper ${prev.electron_20}/bin/electron $out/bin/${oldAttrs.pname} \
-            --set "LOCAL_GIT_DIRECTORY" ${prev.git} \
-            --add-flags $out/share/${oldAttrs.pname}/resources/app \
-            --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
-            --prefix LD_LIBRARY_PATH : "${prev.lib.makeLibraryPath [ prev.stdenv.cc.cc.lib ]}"
-        '';
-      });
-    }
-  )
+    nixpkgs_unstable.logseq
   ];
 
   # Install apps

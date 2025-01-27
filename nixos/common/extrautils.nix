5fe07c244b13a56d4fccf2ba25e3ba6c8ed96ebc
rbbhn <matthew.mccaman@gmail.com>
2025-01-27 14:13:21 -0600

    Add logseq

--- nixos/common/extrautils.nix
+++ nixos/common/extrautils.nix
@@ -3,7 +3,9 @@
     # These packages remain UNCONFIGURED - if needed, separate into modules and configure.
     git
     wget
+
     # Pull progs. out and make individual modules when programs gain home-manager configs
+    # This is the big todo - these progs. should NOT be in this place.
     # TUI
     progress
     lazygit
@@ -17,6 +19,7 @@
     keepassxc
     vlc
     ghostty
+    logseq
   ];
 
   # Install apps
